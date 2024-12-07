edf5c4e53e666fd88ac71aebec19f6458f7da06b
Rohit Jangid <73385164+rohitjangid666@users.noreply.github.com>
2024-12-07 17:48:02 +0530

    feat: download button working

--- src/view/Certificate.jsx
+++ src/view/Certificate.jsx
@@ -1,10 +1,38 @@
+import { useRef } from 'react';
+
 import moment from 'moment';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
 import styles from '../assets/styles/certificateGenerator.module.scss';
 
 const Certificate = ({ fullName, course, dateOfConductStart, dateOfConductEnd, signature, signatureDetails }) => {
+  const certificateRef = useRef(null);
+
+  const handleDownload = async () => {
+    if (!certificateRef.current) {
+      alert('Something went wrong! Please again later');
+      return;
+    }
+
+    try {
+      // use html2canvas to capture a screenshot of the certificate element
+      const canvas = await html2canvas(certificateRef.current);
+      const imgData = canvas.toDataURL('image/png');
+      // create a new jsPDF instance and add the screenshot as an image
+      const pdf = new jsPDF('l', 'mm', [1000, 670]);
+      pdf.addImage(imgData, 'PNG', 0, 0, 1000, 670); // adjust dimension as needed
+
+      // download the pdf file
+      pdf.save(`${fullName.split(' ').join('_')}_certificate`);
+    } catch (error) {
+      console.log(`error: `, error);
+    }
+  };
+
   return (
     <>
-      <div className={styles.certificateWrapper}>
+      <div ref={certificateRef} className={styles.certificateWrapper}>
         <div className={styles.certificateContainer}>
           <div>Logo Here</div>
 
@@ -30,9 +58,11 @@ const Certificate = ({ fullName, course, dateOfConductStart, dateOfConductEnd, s
             <span className={styles.smallText}>{signatureDetails}</span>
           </div>
         </div>
-
-        <button style={{ marginTop: ' 3rem' }}>Download PDF</button>
       </div>
+
+      <button style={{ marginTop: ' 3rem' }} onClick={handleDownload}>
+        Download PDF
+      </button>
     </>
   );
 };
